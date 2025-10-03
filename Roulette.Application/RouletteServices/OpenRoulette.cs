92e004b886d46ab353bd008d16438f545826cfc1
ArnoldFrb <55930519+ArnoldFrb@users.noreply.github.com>
2025-10-03 18:38:24 -0500

    Implementación de servicio para abrir ruletas

---
+++ Roulette.Application/RouletteServices/OpenRoulette.cs
@@ -0,0 +1,32 @@
+﻿using Roulette.Application.Models.Responses;
+using Roulette.Domain.Contracts.Repositories;
+using Roulette.Domain.Contracts.Services;
+
+namespace Roulette.Application.RouletteServices
+{
+    public class OpenRoulette(IRouletteRepository rouletteRepository, IUnitOfWork unitOfWork)
+    {
+        private readonly IRouletteRepository _rouletteRepository = rouletteRepository;
+        private readonly IUnitOfWork _unitOfWork = unitOfWork;
+
+        public RouletteResponse Execute(int rouletteId)
+        {
+            try
+            {
+                var roulette = _rouletteRepository.Find(rouletteId);
+                if (roulette == null)
+                    return new RouletteResponse(null, null, null, "Roulette not found.");
+
+                roulette.OpenBet();
+                _rouletteRepository.Edit(roulette);
+                _unitOfWork.Commit();
+
+                return new RouletteResponse(roulette.Id, roulette.Status.ToString(), roulette.CreatedAt, "Roulette opened successfully.");
+            }
+            catch (Exception ex)
+            {
+                return new RouletteResponse(null, null, null, $"Error opening roulette: {ex.Message}");
+            }
+        }
+    }
+}
